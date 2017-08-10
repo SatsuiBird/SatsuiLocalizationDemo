8f3a4d82fef1cb88d178cb3ae79bb876d0f5cdcc
Satsui Bird <satsuibird@messatsu-dojo.com>
2017-08-10 12:22:21 +0200

    Main solution created

---
+++ DemoWPF/Bootstrap.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace DemoWPF
+{
+    public partial class Bootstrap
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            // No resource need to be cleaned
+            base.OnExit(e);
+        }
+    }
+
+
+}
